ee012f5247d3cf49c33caebdc8f3091cbcbf2b85
Leandro <leandroszikora@est.frba.utn.edu.ar>
2019-01-17 20:46:47 -0300

    Subo readme y query para configurar mongo

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# tpApiRest
+# Trabajo Práctico TP APIRESTful
+
+Se presenta la solución al Trabajo Práctico TP APIRESTful. La misma se trata de una API RESTful para administrar los cursos que son dictados en la empresa.
+
+## Requisitos para ejecutar
+    * MongoDB
+    * Node js
+    * Postman (opcional) 
+
+## Pasos para ejecutar la app
+    * Levantar una instacia de MongoDB Server
+    * Abrir una terminal en la carpeta del repositorio
+    * Ejecutar el comando **mongo capacitacion query.js**
+    * Ejecutar el comando **npm install**
+    * Ejecutar el comando **npm start**
+    * Realizar las requests a traves de Postman o alguna otra aplicacion para dicho proposito (curl, por ejemplo)
---
+++ query.js
@@ -0,0 +1,30 @@
+db.cursos.insert([{
+	anioDictado: 2018,
+	duracion: "3 semanas",
+	tema: "Intro MongoDB",
+	alumnos: [{nombre: "Leandro", apellido: "Szikora", DNI: 40544049, direccion: "Av Victorica 5063", nota:6}, {nombre: "Ignacio", apellido: "Zulo", DNI: 40549362, direccion: "Santos Vega 4898", nota: 7}, {nombre: "Agustin", apellido: "Moles", DNI: 40132987, direccion: "Villa Gerde 5897", nota: 3}]
+},
+{
+	anioDictado: 2019,
+	duracion: "7 semanas",
+	tema: "Curso JavaScript",
+	alumnos: [{nombre: "Leandro", apellido: "Szikora", DNI: 40544049, direccion: "Av Victorica 5063", nota:8}, {nombre: "Ignacio", apellido: "Zulo", DNI: 40549362, direccion: "Santos Vega 4898", nota: 7}, {nombre: "Agustin", apellido: "Moles", DNI: 40132987, direccion: "Villa Gerde 5897", nota: 5}]
+},
+{
+	anioDictado: 2019,
+	duracion: "5 semanas",
+	tema: "Curso Hadoop",
+	alumnos: [{nombre: "Leandro", apellido: "Szikora", DNI: 40544049, direccion: "Av Victorica 5063", nota:4}, {nombre: "Ignacio", apellido: "Zulo", DNI: 40549362, direccion: "Santos Vega 4898", nota: 6}, {nombre: "Agustin", apellido: "Moles", DNI: 40132987, direccion: "Villa Gerde 5897", nota: 8}]
+},
+{
+	anioDictado: 2020,
+	duracion: "3 semanas",
+	tema: "Intro Power BI",
+	alumnos: [{nombre: "Leandro", apellido: "Szikora", DNI: 40544049, direccion: "Av Victorica 5063", nota:4}, {nombre: "Ignacio", apellido: "Zulo", DNI: 40549362, direccion: "Santos Vega 4898", nota:9}, {nombre: "Agustin", apellido: "Moles", DNI: 40132987, direccion: "Villa Gerde 5897", nota: 9}]
+},
+{
+	anioDictado: 2017,
+	duracion: "1 semanas",
+	tema: "Curso Java",
+	alumnos: [{nombre: "Leandro", apellido: "Szikora", DNI: 40544049, direccion: "Av Victorica 5063", nota:9}, {nombre: "Ignacio", apellido: "Zulo", DNI: 40549362, direccion: "Santos Vega 4898", nota: 4}, {nombre: "Agustin", apellido: "Moles", DNI: 40132987, direccion: "Villa Gerde 5897", nota: 6}]
+}])
